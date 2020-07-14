cd01ee8b1076f7c81433cc45e0e50b580660b0f3
Dan Huang <danhuang@verizonmedia.com>
2020-07-14 23:56:50 +0800

    fix: update reponse status when status have not been updated

--- src/UniversalApp.js
+++ src/UniversalApp.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server-express";
 
 import createServerRender from "./createServerRender";
-import { LIB_TAG } from "./config/constants";
+import { LIB_TAG, DEFAULT_STATUS_CODE } from "./config/constants";
 
 const initServer = function(app, routes, apolloServerOptions, production) {
   // mount apollo-server-express middleware
@@ -57,7 +57,12 @@ const initServer = function(app, routes, apolloServerOptions, production) {
                 : context
           })
             .then(html => {
-              res.status(responseStatusCode);
+              if (
+                res.statusCode === DEFAULT_STATUS_CODE &&
+                res.statusCode !== responseStatusCode
+              ) {
+                res.status(responseStatusCode);
+              }
               res.send(html);
             })
             .catch(error => {
